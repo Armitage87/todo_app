09de8942bd4c4e894ea80a0da8ddf362b09ce8b9
Harvey <dez.osk@gmail.com>
2020-03-16 12:32:20 +0000

    adding avatars

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -13,6 +13,16 @@
         </v-btn>
     </v-app-bar>
     <v-navigation-drawer v-model="drawer" app class="indigo">
+    <v-row>
+        <v-col align="center">
+        <v-flex class="mt-5">
+            <v-avatar size="100">
+                <img src="/avatar-1.png" alt="">
+            </v-avatar>
+            <p class="white--text subheading mt-1">The Net ninja</p>
+        </v-flex>
+    </v-col>
+    </v-row>
         <v-list>
             <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
                 <v-list-item-action>
