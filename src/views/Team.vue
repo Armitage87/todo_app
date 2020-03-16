e314c85feb3250d75e38cdcc9b5db10f26eb2979
Harvey <dez.osk@gmail.com>
2020-03-16 00:03:07 +0000

    Adding team member cards

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -2,7 +2,25 @@
   <div class="team">
     <h1 class="subheading grey--text">Team</h1>
     <v-container class="my-5">
-        content
+        <v-row wrap>
+          <v-flex xs12 sm6 md4 lg3 v-for="person in team" :key="person.name">
+            <v-card flat outlined class="text-center ma-3">
+              <v-responsive class="pt-4">
+                image goes here
+              </v-responsive>
+              <v-card-text>
+                <div class="subheading">{{ person.name }}</div>
+                <div class="grey--text">{{ person.role }}</div>
+              </v-card-text>
+              <v-card-actions>
+                <v-btn depressed color="white">
+                  <v-icon small left>message</v-icon>
+                  <span>Message</span>
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-row>
     </v-container>
     
   </div>
@@ -13,6 +31,16 @@
 
 
 export default {
-  name: 'Home'
+  data() {
+    return {
+      team: [
+        { name: 'The Net Ninja', role: 'Web developer' },
+        { name: 'Ryu', role: 'Graphic designer' },
+        { name: 'Chun Li', role: 'Web developer' },
+        { name: 'Gouken', role: 'Social media maverick' },
+        { name: 'Yoshi', role: 'Sales guru'}
+      ]
+    }
+  }
 }
 </script>
